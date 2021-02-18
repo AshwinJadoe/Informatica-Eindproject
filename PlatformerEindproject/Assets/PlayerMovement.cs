e3bc80c9875d5f9c6dd5cd7444e5d6b5675e6a1e
Ashwin <Ashwinjadoe@gmail.com>
2021-02-18 22:26:25 +0100

    player basic movement + gravity werkt nu
    eindelijk

---
+++ PlatformerEindproject/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public float movementSpeed;
+  
+  public Rigidbody2D rb;
+
+  float mx;
+
+  private void Update()
+  {
+    movementSpeed = 3;
+    mx = Input.GetAxisRaw("Horizontal");
+  }
+
+  private void FixedUpdate()
+  {
+    Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+    rb.velocity = movement; 
+  }
+}
