8ab39841d8cf16cbf429243a0c20a2764b49f1ce
Ashwin <Ashwinjadoe@gmail.com>
2021-02-18 21:07:05 +0100

    basic movement function update

--- PlatformerEindproject/Assets/Scenes/Playermovement.cs
+++ PlatformerEindproject/Assets/Scenes/Playermovement.cs
@@ -1,19 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public float moveSpeed = 4f;
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-    Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-    transform.position = transform.position + horizontal * Time.deltaTime; 
-    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //Jump();
+    Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+    transform.position += movement * Time.deltaTime * moveSpeed;
+  }
+
+
+  
 }
