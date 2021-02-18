f8676167afc82067317da7b9446f5819c218b545
Ashwin <Ashwinjadoe@gmail.com>
2021-02-18 23:13:52 +0100

    jump functie aangepast, kan nog double jumpen moet nog gefixt worden

--- PlatformerEindproject/Assets/PlayerMovement.cs
+++ PlatformerEindproject/Assets/PlayerMovement.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class PlayerMovement : MonoBehaviour
 {
   public float movementSpeed;
-
-  public float jumpForce = 5f; 
+  public Transform feet;
+  public float jumpForce = 5f;
+  
   public Rigidbody2D rb;
+  public LayerMask groundLayers;
+
 
   float mx;
 
@@ -16,9 +22,10 @@ public class PlayerMovement : MonoBehaviour
     movementSpeed = 3;
     mx = Input.GetAxisRaw("Horizontal");
 
-    if (Input.GetButtonDown("Jump"))
+    if (Input.GetButtonDown("Jump") && Isgrounded())
     {
       Jump();
+
     }
   }
 
@@ -29,6 +36,16 @@ public class PlayerMovement : MonoBehaviour
     rb.velocity = movement; 
   }
 
+  public bool Isgrounded()
+  {
+    Collider2D groundcheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+    if (groundcheck != null)
+    {
+      return true; 
+    }
+    return false; 
+  }
   void Jump()
   {
     Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
