5c1ec396adb83db8f134bc993865446982d8c71d
Ashwin <Ashwinjadoe@gmail.com>
2021-03-31 16:16:20 +0200

    player controller script aangemaakt spel 2 scene

---
+++ PlatformerEindproject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public float moveSpeed;
+  public float jumpForce;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
