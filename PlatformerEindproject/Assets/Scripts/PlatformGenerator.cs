d0844e2dbce96b3664657a00823e2d5386692ed3
Ashwin <Ashwinjadoe@gmail.com>
2021-03-31 17:29:43 +0200

    platformgenerator voegt nieuwe platformen toe tijdens bewegen

--- PlatformerEindproject/Assets/Scripts/PlatformGenerator.cs
+++ PlatformerEindproject/Assets/Scripts/PlatformGenerator.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+  public GameObject thePlatform;
+  public Transform generationPoint;
+  public float distanceBetween;
+
+  private float platformWidth;
+
+  void Start()
+  {
+    platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if(transform.position.x < generationPoint.position.x)
     {
-        
+      transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, 0);
+
+      Instantiate(thePlatform, transform.position, transform.rotation);
     }
+  }
 }
