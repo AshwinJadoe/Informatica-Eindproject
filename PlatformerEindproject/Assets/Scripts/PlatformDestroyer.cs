266853f57b72c3810abbbe82dad3ab22fdd358e5
Ashwin <Ashwinjadoe@gmail.com>
2021-03-31 18:14:01 +0200

    platform destroyer script aangemaakt, met punt

---
+++ PlatformerEindproject/Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour
+{
+
+  public GameObject platformDestructionPoint;
+  void Start()
+  {
+    platformDestructionPoint = GameObject.Find("PlatformDestructionPoint");
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if(transform.position.x < platformDestructionPoint.transform.position.x)
+    {
+      Destroy(gameObject);
+    }
+  }
+}
