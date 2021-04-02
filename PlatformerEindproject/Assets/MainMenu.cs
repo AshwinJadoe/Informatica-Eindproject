98d81a613a69dc5d8e0376a9a04445f5533a26a8
Robert <38075016+optimusrobertus@users.noreply.github.com>
2021-04-02 16:57:55 +0200

    Werking knoppen in-game pauze menu; beginscherm gelinkt aan correcte scene

--- PlatformerEindproject/Assets/MainMenu.cs
+++ PlatformerEindproject/Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour {
  
     public void PlayGame ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("spel 2");
     }
 
     public void QuitGame ()
--- PlatformerEindproject/Assets/PauseMenu.cs
+++ PlatformerEindproject/Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -23,7 +24,7 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    void Resume ()
+    public void Resume ()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
@@ -35,6 +36,18 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Beginscherm");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
     }
 
 }
